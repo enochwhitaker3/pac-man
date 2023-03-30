7fd4fc155ccf57a753e665a34c204bde445fd07b
Enoch Whitaker <enochwhitaker3@gmail.com>
2023-03-30 11:01:14 -0600

    Updated player movement to not allow player to move inbetween pellets and walls. Movement method was also cleaned up by adding 'updatePlayer'

--- ClassLib/Player.cs
+++ ClassLib/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +20,53 @@ namespace ClassLib
 
         public void initializePlayer(Maze maze)         
         {
-            maze.MazeArray[positionX, positionY] = sprite;                                                 //Adds the players sprite on to the maze in the default position
+            maze.MazeArray[positionX, positionY] = sprite;                                  //Adds the players sprite on to the maze in the default position
         }
+        public void updatePlayer(Maze maze, ConsoleKeyInfo keyInfo)
+        {
+            if (keyInfo.Key == ConsoleKey.RightArrow)                                       //Checks the players input to see if it is the right arrow
+            {
+                if (positionY % 2 == 1)
+                {
+                    maze.MazeArray[positionX, positionY] = ' ';
+                    positionY++;
+                    maze.MazeArray[positionX, positionY] = sprite;
+                }
+                else
+                {
+                    maze.MazeArray[positionX, positionY] = ' ';
+                    positionY += 2;
+                    maze.MazeArray[positionX, positionY] = sprite;
+                }
+            }
+            else if (keyInfo.Key == ConsoleKey.LeftArrow)                                   //Checks the player input to see if it is the left arrow
+            {
+                if (positionY % 2 == 1)
+                {
+                    maze.MazeArray[positionX, positionY] = ' ';
+                    positionY--;
+                    maze.MazeArray[positionX, positionY] = sprite;
+                }
+                else
+                {
+                    maze.MazeArray[positionX, positionY] = ' ';
+                    positionY-=2;
+                    maze.MazeArray[positionX, positionY] = sprite;
+                }
+            }
+            else if (keyInfo.Key == ConsoleKey.UpArrow)                                     //Checks the player input to see if it is the up arrow
+            {
+                maze.MazeArray[positionX, positionY] = ' ';
+                positionX--;
+                maze.MazeArray[positionX, positionY] = sprite;
+            }
+            else if (keyInfo.Key == ConsoleKey.DownArrow)                                   //Checks the player input to see if it is the down arrow
+            {
+                maze.MazeArray[positionX, positionY] = ' ';
+                positionX++;
+                maze.MazeArray[positionX, positionY] = sprite;
+            }
+        }
+            
     }
 }
--- pac-man/Program.cs
+++ pac-man/Program.cs
@@ -7,56 +7,43 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Program a = new();
             Maze maze = new Maze();
             Player player = new Player(maze);
             Ooga ooga = new Ooga(maze);
             Booga booga = new Booga(maze);
             Console.WriteLine(maze.DrawMaze());
      
-
             while (Maze.endGame != true)
             {
-                Movement(player, maze, player.sprite, GetKey());
+                Movement(player, maze, GetKey());
                 Console.WriteLine(maze.DrawMaze());
-                //Maze.endGame = true;
             }
-
         }
         static ConsoleKeyInfo GetKey()
         {
             var keyInfo = Console.ReadKey();
             return keyInfo;
         }
-        public static void Movement(Player player, Maze maze, char sprite, ConsoleKeyInfo keyInfo)
+        public static void Movement(Player player, Maze maze,  ConsoleKeyInfo keyInfo)
         {
-            if (keyInfo.Key == ConsoleKey.RightArrow)                                       //Checks the players input to see if it is the right arrow
+            switch (keyInfo.Key)
             {
-                maze.MazeArray[player.positionX, player.positionY] = ' ';
-                player.positionY++;                                                         
-                maze.MazeArray[player.positionX, player.positionY] = sprite;
-                Console.Clear();
-            }
-            else if (keyInfo.Key == ConsoleKey.LeftArrow)                                   //Checks the player input to see if it is the left arrow
-            {
-                maze.MazeArray[player.positionX, player.positionY] = ' ';
-                player.positionY--;
-                maze.MazeArray[player.positionX, player.positionY] = sprite;
-                Console.Clear();
-            }
-            else if (keyInfo.Key == ConsoleKey.UpArrow)                                     //Checks the player input to see if it is the up arrow
-            {
-                maze.MazeArray[player.positionX, player.positionY] = ' ';
-                player.positionX--;
-                maze.MazeArray[player.positionX, player.positionY] = sprite;
-                Console.Clear();
-            }
-            else if (keyInfo.Key == ConsoleKey.DownArrow)                                   //Checks the player input to see if it is the down arrow
-            {
-                maze.MazeArray[player.positionX, player.positionY] = ' ';
-                player.positionX++;
-                maze.MazeArray[player.positionX, player.positionY] = sprite;
-                Console.Clear();
+                case ConsoleKey.RightArrow:                                                 //Checks the players input to see if it is the right arrow
+                    player.updatePlayer(maze, keyInfo);
+                    Console.Clear();
+                    break;
+                case ConsoleKey.LeftArrow:                                                  //Checks the player input to see if it is the left arrow
+                    player.updatePlayer(maze, keyInfo);                                      
+                    Console.Clear();
+                    break;
+                case ConsoleKey.UpArrow:                                                    //Checks the player input to see if it is the up arrow
+                    player.updatePlayer(maze, keyInfo);                                      
+                    Console.Clear();
+                    break;
+                case ConsoleKey.DownArrow:                                                  //Checks the player input to see if it is the down arrow
+                    player.updatePlayer(maze, keyInfo);                                  
+                    Console.Clear();
+                    break;
             }
         }
     }
