fe5ead8e8f88e21b5b20489af1c177a595657c28
enochwhitaker3 <enochwhitaker3@gmail.com>
2023-04-10 15:38:38 -0600

    Goofy animation prototype added, most likely not permanent

--- pac-man/Program.cs
+++ pac-man/Program.cs
@@ -12,10 +12,12 @@ namespace MyApp
             Player player = new Player(maze);
             Ooga ooga = new Ooga(maze);
             Booga booga = new Booga(maze);
-            Console.WriteLine(maze.DrawMaze());
+            //Console.WriteLine(maze.DrawMaze());
      
             while (Maze.endGame != true)
             {
+                Console.WriteLine("8b,dPPYba,  ,adPPYYba,  ,adPPYba, 88,dPYba,,adPYba,  ,adPPYYba, 8b,dPPYba,   \r\n88P'    \"8a \"\"     `Y8 a8\"     \"\" 88P'   \"88\"    \"8a \"\"     `Y8 88P'   `\"8a  \r\n88       d8 ,adPPPPP88 8b         88      88      88 ,adPPPPP88 88       88  \r\n88b,   ,a8\" 88,    ,88 \"8a,   ,aa 88      88      88 88,    ,88 88       88  \r\n88`YbbdP\"'  `\"8bbdP\"Y8  `\"Ybbd8\"' 88      88      88 `\"8bbdP\"Y8 88       88  \r\n88                                                                           \r\n88                 ");
+                loadingScreen(GetKey());
                 Console.Write($"SCORE : {player.score}\n");
                 Movement(player, maze, GetKey());
                 Console.WriteLine(maze.DrawMaze());
@@ -46,6 +48,84 @@ namespace MyApp
                     player.updatePlayer(maze, keyInfo);                                  
                     Console.Clear();
                     break;
+            }
+        }
+        public static void loadingScreen(ConsoleKeyInfo keyInfo)
+        {
+            while (keyInfo.Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine("<......");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine(" <.....");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("B <....");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine(" B <...");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("O B <..");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine(" O B <.");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("  O B <");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("   O B ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("    O B");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("     O ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("      O");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("       ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("      >");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("     > ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("    > B");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("   > B ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("  > B O");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine(" > B O ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("> B O  ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine(" B O   ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("B O    ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine(" O     ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("O      ");
+                Thread.Sleep(400);
+                Console.Clear();
+                Console.WriteLine("      ");
+                Thread.Sleep(400);
+                Console.Clear();
             }
         }
     }
